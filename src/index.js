fcf2ac7779005470b65c6ba503e9cb64bffaa422
kaykakaraka <23278323+kaykakaraka@users.noreply.github.com>
2023-08-23 17:05:12 +0100

    Exported board array with square objects to a different file to simplify index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
+import boardArray from './boardArray.js'
 
 class Square extends React.Component {
   constructor(props) {
@@ -39,116 +40,7 @@ class Game extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      board: [
-        {
-          position: 1,
-          input: '3',
-          content: '3',
-          answer: '3',
-        },
-        {
-          position: 2,
-          input: '',
-          content: '',
-          answer: '7'
-        },
-        {
-          position: 3,
-          input: '',
-          content: '',
-          answer: '4'
-        },
-        {
-          position: 4,
-          input: '',
-          content: '',
-          answer: '6'
-        },
-        {
-          position: 5,
-          input: '',
-          content: '',
-          answer: '5'
-        },
-        {
-          position: 6,
-          input: '',
-          content: '',
-          answer: '1'
-        },
-        {
-          position: 7,
-          input: '',
-          content: '9',
-          answer: '9'
-        },
-        {
-          position: 8,
-          input: '',
-          content: '8',
-          answer: '8'
-        },
-        {
-          position: 9,
-          input: '',
-          content: '',
-          answer: '2'
-        },
-        {
-          position: 10,
-          input: '',
-          content: '',
-          answer: '9'
-        },
-        {
-          position: 11,
-          input: '',
-          content: '',
-          answer: '6'
-        },
-        {
-          position: 12,
-          input: '',
-          content: '2',
-          answer: '2'
-        },
-        {
-          position: 13,
-          input: '',
-          content: '7',
-          answer: '7'
-        },
-        {
-          position: 14,
-          input:'',
-          content: '',
-          answer: '3'
-        },
-        {
-          position: 15,
-          input: '',
-          content: '',
-          answer: '8'
-        }, 
-        {
-          position: 16,
-          input: '',
-          content: '4',
-          answer: '4'
-        },
-        {
-          position: 17,
-          input: '',
-          content: '5',
-          answer: '5'
-        },
-        {
-          position: 18,
-          input: '',
-          content: '5',
-          answer: '5'
-        }
-      ]
+      board: boardArray
     }
   }
 
