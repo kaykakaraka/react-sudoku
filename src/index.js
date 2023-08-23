68bf37befa88464ebe2296efba780ba3671d8cdd
kaykakaraka <23278323+kaykakaraka@users.noreply.github.com>
2023-08-23 16:46:27 +0100

    Replaced board array with an array of objects, containing the position, a blank input, the content the square starts with and the ultimate answer. Two rows completed

--- src/index.js
+++ src/index.js
@@ -22,7 +22,8 @@ class Board extends React.Component {
   render() {
     return (
       <div className='board'>
-        {this.props.board.map((item) => <Square content={item}/>)}
+        {this.props.board.map((item) => <Square content={item.content}/>)}
+        {console.log(this.props.board)}
           {<Square />}
       </div>
     ) 
@@ -34,16 +35,108 @@ class Game extends React.Component {
     super(props)
     this.state = {
       board: [
-        ' ', ' ', ' ', ' ', ' ', '1', ' ', ' ', ' ',
-        ' ', ' ', ' ', ' ', ' ', ' ', '1', ' ', ' ',
-        ' ', '1', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-        ' ', ' ', ' ', ' ', ' ', '3', ' ', ' ', ' ',
-        ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-        ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-        ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-        ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-        ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
-      ],
+        {
+          position: 1,
+          input: '3',
+          content: '3',
+          answer: '3',
+        },
+        {
+          position: 2,
+          input: '',
+          content: '',
+          answer: '7'
+        },
+        {
+          position: 3,
+          input: '',
+          content: '',
+          answer: '4'
+        },
+        {
+          position: 4,
+          input: '',
+          content: '',
+          answer: '6'
+        },
+        {
+          position: 5,
+          input: '',
+          content: '',
+          answer: '5'
+        },
+        {
+          posiiton: 6,
+          input: '',
+          content: '',
+          answer: '1'
+        },
+        {
+          position: 7,
+          input: '',
+          content: '9',
+          answer: '9'
+        },
+        {
+          position: 8,
+          input: '',
+          content: '8',
+          answer: '8'
+        },
+        {
+          position: 9,
+          input: '',
+          content: '',
+          answer: '2'
+        },
+        {
+          position: 10,
+          input: '',
+          content: '',
+          answer: '9'
+        },
+        {
+          position: 11,
+          input: '',
+          content: '',
+          answer: '6'
+        },
+        {
+          position: 12,
+          input: '',
+          content: '2',
+          answer: '2'
+        },
+        {
+          position: 13,
+          input: '',
+          content: '7',
+          answer: '7'
+        },
+        {
+          position: 14,
+          input:'',
+          content: '',
+          answer: '3'
+        },
+        {
+          position: 15,
+          input: '',
+          content: '',
+          answer: '8'
+        }, 
+        {
+          position: 16,
+          input: '',
+          content: '4',
+          answer: '4'
+        },
+        {
+          position: 17,
+          input: '',
+          content: '5',
+          answer: '5'
+        }
     }
   }
 
